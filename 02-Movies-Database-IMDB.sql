98c86dd4aacd1359a93ed242522d600bddea217e
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-03 11:06:17 +0530

    Innovation - 03-Jan

--- 02-Movies-Database-IMDB.sql
+++ 02-Movies-Database-IMDB.sql
@@ -1,3 +1,6 @@
+=============================================================================================
+Retrieve Data Using Text Query (SELECT, WHERE, DISTINCT, LIKE)
+
 SELECT * from moviesdb.movies;
 SELECT title, industry FROM moviesdb.movies;
 use moviesdb;
@@ -52,6 +55,9 @@ SELECT * from movies where imdb_rating IS NULL;
 -- All movies where imdb rating is available 
 SELECT * from movies where imdb_rating IS NOT NULL;
 
+
+=============================================================================================
+Retrieve Data Using Numeric Query (BETWEEN, IN, ORDER BY, LIMIT, OFFSET)
 -- Print all bollywood movies ordered by their imdb rating
 SELECT * From movies where industry = "bollywood"
 order by imdb_rating desc;
@@ -87,3 +93,58 @@ select title, release_year from movies
 
 -- select all movies that are not from marvel studios
 select * from movies where studio!="marvel studios"
+
+=============================================================================================
+Summary Analytics (MIN, MAX, AVG, GROUP BY)
+
+-- How many total movies do we have in our movies table?
+SELECT COUNT(*) from movies;
+	
+-- Select highest imdb rating for bollywood movies
+SELECT MAX(imdb_rating) from movies where industry="Bollywood";
+
+-- Select lowest imdb rating for bollywood movies
+SELECT MIN(imdb_rating) from movies where industry="Bollywood";
+
+-- Print average rating of Marvel Studios movies
+SELECT AVG(imdb_rating) from movies where studio="Marvel Studios";
+SELECT ROUND(AVG(imdb_rating),2) from movies where studio="Marvel Studios";
+
+-- Print min, max, avg rating of Marvel Studios movies
+	SELECT 
+           MIN(imdb_rating) as min_rating, 
+           MAX(imdb_rating) as max_rating, 
+           ROUND(AVG(imdb_rating),2) as avg_rating
+        FROM movies 
+        WHERE studio="Marvel Studios";
+        
+-- Print count of movies by industry
+	SELECT 
+           industry, count(industry) 
+        FROM movies
+        GROUP BY industry;
+-- Same thing but add average rating
+	SELECT 
+            industry, 
+            count(industry) as movie_count,
+            avg(imdb_rating) as avg_rating
+	FROM movies
+	GROUP BY industry;
+
+-- Count number of movies released by a given production studio
+	SELECT 
+	    studio, count(studio) as movies_count 
+        from movies WHERE studio != ''
+	GROUP BY studio
+	ORDER BY movies_count DESC;
+
+-- What is the average rating of movies per studio and also order them by average rating in descending format?
+	SELECT 
+	   studio, 
+	   count(studio) as cnt, 
+	   round(avg(imdb_rating), 1) as avg_rating 
+	from movies WHERE studio != ''
+	GROUP BY studio
+        order by avg_rating DESC;
+
+=============================================================================================

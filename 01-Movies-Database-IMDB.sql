48126d9bf1f5c324d0fbd19fc2ee6c767abf38b8
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-06 06:54:58 +0530

    Resilience - 06-Jan

--- 01-Movies-Database-IMDB.sql
+++ 01-Movies-Database-IMDB.sql
@@ -148,3 +148,39 @@ SELECT ROUND(AVG(imdb_rating),2) from movies where studio="Marvel Studios";
         order by avg_rating DESC;
 
 =============================================================================================
+Module: Calculated Columns (IF, CASE, YEAR, CURYEAR)
+
+-- Print actor name, their birth_year and age
+	SELECT 
+           name, birth_year, (YEAR(CURDATE())-birth_year) as age
+	FROM actors;
+
+-- Print profit for every movie
+	SELECT 
+	    *, 
+           (revenue-budget) as profit 
+	from financials;
+
+-- Print revenue of all movies in INR currency
+	SELECT 
+           movie_id, 
+	   revenue, 
+           currency, 
+           unit,
+           IF (currency='USD', revenue*77, revenue) as revenue_inr
+	FROM financials;
+
+-- Get all the unique units from financial table
+	select 
+	   distinct unit 
+	From financials;
+
+-- Print revenue in millions 
+	SELECT 
+           movie_id, revenue, currency, unit,
+           CASE
+              WHEN unit="Thousands" THEN revenue/1000
+              WHEN unit="Billions" THEN revenue*1000
+             ELSE revenue
+           END as revenue_mln
+	FROM financials
